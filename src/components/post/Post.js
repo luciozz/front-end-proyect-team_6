b5d8a035cd069a1b63ad836baff5c4c5d58880bd
Aleee D <aleee.datos@gmail.com>
2022-04-29 00:47:30 -0300

    Funciona el contenido HTML

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -4,12 +4,13 @@ import React from "react"
 export function APost(props) {
     console.log(props)
     console.log(props.aPostData.id)
+    let content = props.aPostData.content.rendered
     if(props.aPostData.id>0){
         return (
             <div className="apost"> 
                 <a href={props.aPostData.link}><h1>{props.aPostData.title.rendered}</h1></a>
                 <p>{props.aPostData.date}</p>
-                <p>{props.aPostData.content.rendered}</p>
+                <div  dangerouslySetInnerHTML={{__html: content}}></div>
             </div>
         )
     }else{
