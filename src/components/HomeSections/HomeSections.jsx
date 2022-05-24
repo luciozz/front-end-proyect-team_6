479e9404f323603802243c115b9a38203106993c
maximilianoDobal <maximilianodobal@gmail.com>
2022-05-24 16:19:12 -0300

    arreglo de home, svg y homeSections

--- src/components/HomeSections/HomeSections.jsx
+++ src/components/HomeSections/HomeSections.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import "./HomeSections.css";
 
 const HomeSections = ({ svg, txt, change }) => {
-  return (    
-    <div className="grid grid-cols-1 text-center items-center gap-y-8 gap-x-8 md:grid-cols-2 py-12 container mx-auto ">
+  return (
+    <div className="sectionContainer grid grid-cols-1 text-center items-center gap-y-8 gap-x-8 md:grid-cols-2 py-12 my-5 container mx-auto border-orange-600 border dark:bg-gray-800 mt-5 rounded-lg dark:bg-gradient-to-r dark:from-gray-900 dark:hover:bg-orange-600 transition ease-in-out hover:scale-95 hover:shadow-[0_8px_8px_1px_rgba(0,0,0,0.5)] duration-500">
       <div className={`relative ${change ? "md:order-2" : "md:order-1"}`}>
-        <p className={`text-5xl md:text-6xl font-bold md:order-3 dark:text-slate-100`}>{txt}</p>
+        <p className={`text-5xl md:text-6xl font-bold md:order-3 dark:text-slate-100`}>
+          {txt}
+        </p>
       </div>
-      {svg}
+      <div className={`container-svg ${change ? "md:order-1" : "md:order-2"}`}>{svg}</div>
     </div>
   );
 };
